1c823f494148d7669c5c0f225a2e6b435801679c
kaukometsahovi <115698544+kaukometsahovi@users.noreply.github.com>
2022-10-13 13:59:37 +0300

    Update streamlit_app.py
    Virheenkorjaus

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('my Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-🥑🍞
+
 streamlit.text('🥣 Omega 3 and Blueberry outmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range Egg')
-          
+streamlit.text('🥑🍞 Avocado toast')              
