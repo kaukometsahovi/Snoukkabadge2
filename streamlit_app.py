6e5259f096f60af78934abf2087a35fda966353f
kaukometsahovi <115698544+kaukometsahovi@users.noreply.github.com>
2022-10-13 12:13:34 +0300

    Update streamlit_app.py
    Lisää tekstiä

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.title('my Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 and Blueberry outmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
+               
