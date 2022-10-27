03175201974245a073780c49d9f500845299ac00
kaukometsahovi <115698544+kaukometsahovi@users.noreply.github.com>
2022-10-27 10:31:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,30 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # 26.10.2022 Jatketaan
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choic:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)   
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())   
+    streamlit.dataframe(fruityvice_normalized)
 
+except ERLError as e:
+   streamlit.error()
+  
+
+####SIIVOTAAN
+#treamlit.write('The user entered ', fruit_choice)
+  
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 # Ei tarvita - streamlit.text(fruityvice_response.json())
 
 # Hakee rivit
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # Tekee kehyjset
-streamlit.dataframe(fruityvice_normalized)
+####SIIVOUS END
 
 #Koodin debuggaus, stoppi
 streamlit.stop()
