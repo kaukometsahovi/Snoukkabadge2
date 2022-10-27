32434005d52517f8972ca3bd382c6cce635f97fd
kaukometsahovi <115698544+kaukometsahovi@users.noreply.github.com>
2022-10-27 11:51:06 +0300

    Update streamlit_app.py
    Funktiotreeni

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+#Functiontreeni
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)   
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+#funktiotreeni end
+
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 # 26.10.2022 Jatketaan
@@ -31,9 +38,11 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)   
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())   
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    # Vanhaa fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)   
+    # vanhaa fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())   
+    # vanhaa streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
    streamlit.error()
