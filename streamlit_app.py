0c71cffcd30e59acb9876e94230e0d4fb2abdf3a
kaukometsahovi <115698544+kaukometsahovi@users.noreply.github.com>
2022-10-27 10:20:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,6 @@ streamlit.dataframe(my_data_rows)
 #Toka boksi
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#paska testi
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
